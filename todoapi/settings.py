713898447b023b666a3da287b8a0422916ffa7b1
anniraz <zarinakudajberdikyzy@gmail.com>
2022-12-22 02:41:10 +0600

    added swagger

--- todoapi/settings.py
+++ todoapi/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'drf_yasg',
     'rest_framework',
     "rest_framework_simplejwt",
     "phonenumber_field",
