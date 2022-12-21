61e87b9a10de1a0ea2c57b51c2597bb587aff15d
anniraz <zarinakudajberdikyzy@gmail.com>
2022-12-22 02:34:30 +0600

    additional commit

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',UserListCreateApiView.as_view())
+    path('',UserListCreateApiView.as_view()),
+    path('detail/<int:pk>/',UserDetailApiView.as_view()),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -2,10 +2,15 @@ from rest_framework import generics
 
 from apps.user.models import User
 from apps.user.serializers import UserCreateSerializer
+from apps.user.permissions import IsOwner
 
 
 class UserListCreateApiView(generics.ListCreateAPIView):
     queryset=User.objects.all()
     serializer_class=UserCreateSerializer
 
-    
+
+class UserDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=User.objects.all()
+    serializer_class=UserCreateSerializer 
+    permission_classes=[IsOwner]
