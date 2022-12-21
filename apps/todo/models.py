1b0bb1a6261e614395f7cee2bf9e3267b981be4a
anniraz <zarinakudajberdikyzy@gmail.com>
2022-12-22 00:40:22 +0600

    todo models updated

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from apps.user.models import User
+
 
 class Todo(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     title=models.CharField(max_length=255, unique=True)
     description=models.TextField()
     is_completed=models.BooleanField(default=False)
