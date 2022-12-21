09e3aba4b9b368423e1dbcdd00e44ce49bc88093
anniraz <zarinakudajberdikyzy@gmail.com>
2022-12-21 22:55:32 +0600

    created  models

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title=models.CharField(max_length=255)
+    description=models.TextField()
+    is_completed=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    image=models.ImageField(upload_to='todo_img/')
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username=models.CharField(max_length=255)
+    email=models.EmailField()
+    phone_number=models.CharField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    age=models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.username
