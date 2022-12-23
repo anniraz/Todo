0003d1b2a21eda5e564bd5ec2e7dc4b88a2bf5ed
anniraz <zarinakudajberdikyzy@gmail.com>
2022-12-23 21:53:17 +0600

    corsheaders added

--- todoapi/settings.py
+++ todoapi/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-5cqui(mrn*#5m$1jk59m$d_r7+6j%yk!zwneox0h4wh9s0jbuc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
